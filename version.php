2daad0bc45360c5fd411be29d0b6be5ba1b5ee0a
John McGettrick <jmcgettrick@iparadigms.com>
2014-07-30 11:54:34 +0100

    Warning on Version page if $plugin is undefined removed.

--- version.php
+++ version.php
@@ -11,6 +11,10 @@ $module->maturity  = MATURITY_STABLE;
 $module->release  = '2.3+';
 $module->cron      = 1800;        // Period for cron to check this module in seconds
 
+if (empty($plugin)) {
+	$plugin = new StdClass();
+}
+
 $plugin->version   = 2014012405;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012062500;
 $plugin->component = 'mod_turnitintooltwo';
