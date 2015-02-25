aaeccab19767de8369c686006550a26a883e349f
John McGettrick <jmcgettrick@iparadigms.com>
2015-02-25 12:13:08 +0000

    Set save to true if part has changed.

--- turnitintooltwo_submission.class.php
+++ turnitintooltwo_submission.class.php
@@ -609,7 +609,7 @@ class turnitintooltwo_submission {
         // If save not passed in then only update if certain items have changed to save on database load.
         if ($this->submission_grade != $sub->submission_grade || $this->submission_score != $sub->submission_score ||
             $this->submission_modified != $sub->submission_modified || $this->submission_attempts != $sub->submission_attempts ||
-            $this->submission_unanon != $sub->submission_unanon) {
+            $this->submission_unanon != $sub->submission_unanon || $this->submission_part != $sub->submission_part) {
             $save = true;
         }
 
