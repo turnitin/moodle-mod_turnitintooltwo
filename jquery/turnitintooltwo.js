82268ebc81251499ee14f28f0ad265073b4c6cd9
David Dyke <ddyke@iparadigms.com>
2015-03-24 11:07:25 +0000

    Momentjs needs you to specify the format of the date if it's not ISO 8601 formatted

--- jquery/turnitintooltwo.js
+++ jquery/turnitintooltwo.js
@@ -1229,6 +1229,6 @@ jQuery(document).ready(function($) {
         });
 
         // return converted moment object
-        return moment(date).unix();
+        return moment(date, "DD-MMMM-YYYY hh:mm").unix();
     }
 });
