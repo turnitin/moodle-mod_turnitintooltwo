76bf69267f0559ac0c57de97f546a288a5a1e1c2
John McGettrick <jmcgettrick@iparadigms.com>
2015-07-15 14:47:48 +0100

    Add additional date for pp resubmissions.

--- jquery/plagiarism_plugin.js
+++ jquery/plagiarism_plugin.js
@@ -109,12 +109,15 @@ jQuery(document).ready(function($) {
         var that = $(this);
 
         var submissionid = $(this).prop('id').split("_")[2];
+        var forumpost = $('#content_'+submissionid).html();
+        var forumdata = $('#forumdata_'+submissionid).html();
 
         $.ajax({
             type: "POST",
             url: M.cfg.wwwroot +"/plagiarism/turnitin/ajax.php",
             dataType: "json",
-            data: {action: "resubmit_event", submissionid: submissionid, sesskey: M.cfg.sesskey},
+            data: {action: "resubmit_event", submissionid: submissionid, forumpost: forumpost,
+                    forumdata: forumdata, sesskey: M.cfg.sesskey},
             success: function(data) {
                 that.siblings('.turnitin_status').removeClass('hidden');
                 that.siblings('.pp_resubmitting').addClass('hidden');
