6dfec35d663c3a3f8f55558773c4441c156e53cc
John McGettrick <jmcgettrick@iparadigms.com>
2015-07-27 12:25:15 +0100

    Change intro editor for Moodle 2.9 compatibility.

--- mod_form.php
+++ mod_form.php
@@ -190,7 +190,12 @@ class mod_turnitintooltwo_mod_form extends moodleform_mod {
         $mform->addRule('name', get_string('maxlength', 'turnitintooltwo', $input), 'maxlength', 40, 'client');
         $mform->addRule('name', get_string('maxlength', 'turnitintooltwo', $input), 'maxlength', 40, 'server');
 
-        $this->add_intro_editor(true, get_string('turnitintooltwointro', 'turnitintooltwo'));
+        if ($CFG->branch >= 29) {
+            $this->standard_intro_elements(get_string('turnitintooltwointro', 'turnitintooltwo'));
+        } else {
+            $this->add_intro_editor(true, get_string('turnitintooltwointro', 'turnitintooltwo'));
+        }
+
         $typeoptions = turnitintooltwo_filetype_array(true);
 
         $mform->addElement('select', 'type', get_string('type', 'turnitintooltwo'), $typeoptions);
