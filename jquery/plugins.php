c8725cfa0184c884e4883bf40a6a2a3bb57e3dfe
John McGettrick <jmcgettrick@iparadigms.com>
2015-08-14 15:17:09 +0100

    Remove plagiarism plugin js file

--- jquery/plugins.php
+++ jquery/plugins.php
@@ -26,6 +26,5 @@ $plugins = array(
 	'turnitintooltwo-uieditable'				=> array('files' => array('jqueryui-editable.js', 'jqueryui-editable.css')),
 	'turnitintooltwo-moment'					=> array('files' => array('moment.js')),
 	'turnitintooltwo-block'						=> array('files' => array('block_turnitin.js')),
-	'turnitintooltwo-plagiarism_plugin'			=> array('files' => array('plagiarism_plugin.js')),
 	'turnitintooltwo-tooltipster'				=> array('files' => array('tooltipster.js', 'tooltipster.css'))
 );
