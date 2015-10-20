5bb905b2aeddf500fcf13f3b1650cda04265301e
David Hatton <dhatton@iparadigms.com>
2015-10-20 15:22:45 +0100

    changes made to CSS and turnitintooltwo_view to make rubric logo visible in student view.

--- turnitintooltwo_view.class.php
+++ turnitintooltwo_view.class.php
@@ -100,11 +100,11 @@ class turnitintooltwo_view {
             $PAGE->requires->js($jsurl, true);
             $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/jquery.dataTables.plugins.js');
             $PAGE->requires->js($jsurl, true);
-            $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/turnitintooltwo.js');
+            $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/turnitintooltwo.min.js');
             $PAGE->requires->js($jsurl, true);
-            $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/turnitintooltwo_extra.js');
+            $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/turnitintooltwo_extra.min.js');
             $PAGE->requires->js($jsurl, true);
-            $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/turnitintooltwo_settings.js');
+            $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/turnitintooltwo_settings.min.js');
             $PAGE->requires->js($jsurl, true);
             $jsurl = new moodle_url('/mod/turnitintooltwo/jquery/jquery.dataTables.columnFilter.js');
             $PAGE->requires->js($jsurl, true);
@@ -534,6 +534,12 @@ class turnitintooltwo_view {
         $tabitems = array();
         $i = 0;
 
+        // Determine which tab position to enable after a submission deletion.
+        $tab_position = array_search(optional_param('partid', 0, PARAM_INT), array_keys($partdetails));
+        if ($tab_position) {
+            $output .= html_writer::tag('div', $tab_position, array('id' => 'tab_position', 'class' => 'hidden_class'));
+        }
+        
         foreach ($partdetails as $partid => $partobject) {
             if (!empty($partid)) {
                 $i++;
@@ -777,13 +783,13 @@ class turnitintooltwo_view {
         $cells[3] = new html_table_cell($datefield);
         $cells[3]->attributes['class'] = 'data';
 
-        // Show Rubric view if applicable to students.
+        // Show Rubric view if applicable to students. 
         $rubricviewlink = '';
         if (!$istutor && $config->usegrademark && !empty($turnitintooltwoassignment->turnitintooltwo->rubric)) {
             $rubricviewlink .= $OUTPUT->box_start('row_rubric_manager', '');
             $rubricviewlink .= html_writer::link($CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.
-                                                    '&part='.$partid.'&do=rubricview&view_context=box', 
-                                                    html_writer::tag('span', '', array('class' => 'tiiicon icon-rubric icon-lg', 'id' => 'rubric_view_form')),
+                                                    '&part='.$partid.'&do=rubricview&view_context=box',
+                                                html_writer::tag('span', '', array('class' => 'tiiicon icon-rubric icon-lg', 'id' => 'rubric_view_form')),
                                                 array('class' => 'rubric_view_launch', 'id' => 'rubric_view_launch',
                                                     'title' => get_string('launchrubricview', 'turnitintooltwo')));
             $rubricviewlink .= $OUTPUT->box_end(true);
@@ -1373,7 +1379,7 @@ class turnitintooltwo_view {
 
                 $attributes = array("onclick" => "return confirm('".$string."');");
                 $delete = html_writer::link(
-                    $CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.'&action=deletesubmission&sub='.$submission->id.'&sesskey='.sesskey(),
+                    $CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.'&part='.$partid.'&action=deletesubmission&sub='.$submission->id.'&sesskey='.sesskey(),
                     html_writer::tag('i', '', array('class' => 'fa fa-trash-o fa-lg')),
                     $attributes
                 );
@@ -1386,7 +1392,7 @@ class turnitintooltwo_view {
 
                 $attributes = array("onclick" => "return confirm('".$string."');");
                 $delete = html_writer::link(
-                    $CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.'&action=deletesubmission&sub='.$submission->id.'&sesskey='.sesskey(),
+                    $CFG->wwwroot.'/mod/turnitintooltwo/view.php?id='.$cm->id.'&part='.$partid.'&action=deletesubmission&sub='.$submission->id.'&sesskey='.sesskey(),
                     html_writer::tag('i', '', array('class' => 'fa fa-trash-o fa-lg')),
                     $attributes
                 );
