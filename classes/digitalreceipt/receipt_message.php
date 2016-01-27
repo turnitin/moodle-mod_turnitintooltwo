5bc635627f3042cc9283da00125e59f8d4cf57ae
David Hatton <dhatton@iparadigms.com>
2016-01-27 10:23:16 +0000

    Change no reply address to no reply ID to match expected input.

--- classes/digitalreceipt/receipt_message.php
+++ classes/digitalreceipt/receipt_message.php
@@ -9,12 +9,14 @@ class receipt_message {
      * @return void
      */
     public function send_message($userid, $message) {
+        global $CFG;
+
         $subject = get_string('digital_receipt_subject', 'turnitintooltwo');
 
         $eventdata = new stdClass();
         $eventdata->component         = 'mod_turnitintooltwo'; //your component name
         $eventdata->name              = 'submission'; //this is the message name from messages.php
-        $eventdata->userfrom          = $CFG->noreplyaddress;
+        $eventdata->userfrom          = $CFG->noreplyuserid;
         $eventdata->userto            = $userid;
         $eventdata->subject           = $subject;
         $eventdata->fullmessage       = $message;
