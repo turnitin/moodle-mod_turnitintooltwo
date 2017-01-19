9dceb68f11d294f5c7464605aea5406209da3053
James Robson <jrobson@turnitin.com>
2017-01-19 16:55:24 +0000

    Added a build message test for multipart

--- tests/unit/classes/digitalreceipt/receipt_message_test.php
+++ tests/unit/classes/digitalreceipt/receipt_message_test.php
@@ -65,29 +65,22 @@ class mod_turnitintooltwo_receipt_message_testcase extends advanced_testcase {
     }
 
     public function test_build_message_multi_part() {
-        global $DB;
-
-        $this->resetAfterTest();            // Reset the DB when finished
-        $this->preventResetByRollback();    // Messaging doesn't support rollback
-
-        $sink = $this->redirectMessages();  // Collect the emails
-
-        $userOne = $this->getDataGenerator()->create_user();
-
         $receipt_message = new receipt_message();
 
-        $receipt_message->send_message($userOne, "Test message for email");
+        $date = date('c');
 
-        $this->assertEquals(1, $sink->count()); // One email sent
+        $message = [];
+        $message['firstname']        = 'test_user_firstname';
+        $message['lastname']         = 'test_user_lastname';
+        $message['submission_title'] = 'test submission title';
+        $message['assignment_name']  = 'test assignment name';
+        $message['assignment_part']  = 'assignment_part';
+        $message['course_fullname']  = 'test course name';
+        $message['submission_date']  = $date;
+        $message['submission_id']    = '12345';
 
-        $messages = $sink->get_messages();
+        $response = $receipt_message->build_message($message);
 
-        // Correct user was sent an email
-        $this->assertEquals($userOne->id, $messages[0]->useridto);
-        $this->assertEquals("This is your Turnitin Digital Receipt", $messages[0]->subject);
-        $this->assertEquals("Test message for email", $messages[0]->fullmessage);
-        $this->assertEquals("Test message for email", $messages[0]->fullmessagehtml);
-        $this->assertEquals(1, $messages[0]->fullmessageformat); // HTML format
+        $this->assertEquals("Dear " . $message['firstname'] . " " . $message['lastname'] . ",<br /><br />You have successfully submitted the file <strong>" . $message['submission_title'] . "</strong> to the assignment <strong>" . $message['assignment_name'] . ": " . $message['assignment_part'] . "</strong> in the class <strong>" . $message['course_fullname'] . "</strong> on <strong>" . $date . "</strong>. Your submission id is <strong>" . $message['submission_id'] . "</strong>. Your full digital receipt can be viewed and printed from the assignment inbox or from the print/download button in the document viewer.<br /><br />Thank you for using Turnitin,<br /><br />The Turnitin Team", $response);
     }
-
 }
