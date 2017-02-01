b05de8f2e88aa738a6c3acc4206872834894cf7c
Tobias Gray <tobias.gray@outlook.com>
2017-02-01 15:24:53 +0000

    Added some comments to members view test

--- tests/unit/classes/view/members_test.php
+++ tests/unit/classes/view/members_test.php
@@ -48,57 +48,81 @@ class mod_turnitintooltwo_view_members_testcase extends externallib_advanced_tes
         $this->assertEquals('<div id="general" class="box generalbox boxaligncenter">The selected Tutors below are enrolled as tutors on this Turnitin Class. Enrolled tutors can gain access to this class by logging in to the Turnitin web site.</div>', $message);
     }
 
+    /**
+     * Test given a display role the correct table HTML is generated
+     */
     public function test_build_members_table() {
+        // fake/stub a turnitin two view class and the method to render the
+        // table
         $observer = $this->getMockBuilder(turnitintooltwo_view::class)
-             ->setMethods(['init_tii_member_by_role_table'])
-             ->getMock();
+            ->setMethods(['init_tii_member_by_role_table'])
+            ->getMock();
 
+        // add assertions to the turnitin two view class method that renders the
+        // members table is called with the expected arguments
         $observer->expects($this->exactly(3))
-             ->method('init_tii_member_by_role_table')
-             ->willReturn('<table>fake table!</table>')
-             ->withConsecutive(
+            ->method('init_tii_member_by_role_table')
+            ->willReturn('<table>fake table!</table>')
+            ->withConsecutive(
                 [$this->equalTo('fakemodule'), $this->equalTo('faketiiassignment'), $this->equalTo('Learner')],
                 [$this->equalTo('fakemodule'), $this->equalTo('faketiiassignment'), $this->equalTo('Instructor')],
                 [$this->equalTo('fakemodule'), $this->equalTo('faketiiassignment'), $this->equalTo('Learner')]
             );
 
+        // create out members view instance passing our stubbed turnitin two
+        // view class instance
         $members = new members_view('fakecourse', 'fakemodule', $observer, 'faketiiassignment');
 
+        // check with valid Learner role
         $table = $members->build_members_table('Learner');
         $this->assertEquals('<table>fake table!</table>', $table);
 
+        // check with valid Instructor role
         $table = $members->build_members_table('Instructor');
         $this->assertEquals('<table>fake table!</table>', $table);
 
+        // check no role falls back to Learner
         $table = $members->build_members_table();
         $this->assertEquals('<table>fake table!</table>', $table);
     }
 
+    /**
+     * Test given a display role the correct add tutors form is generated.
+     */
     public function test_build_add_tutors_form() {
+        // fake/stub a turnitin two view class and method to render the add
+        // tutors form
         $faketiiview = $this->getMockBuilder(turnitintooltwo_view::class)
-             ->setMethods(['show_add_tii_tutors_form'])
-             ->getMock();
+            ->setMethods(['show_add_tii_tutors_form'])
+            ->getMock();
 
+        // add assertions that the generate add tutors form is called as
+        // expected
         $faketiiview->expects($this->once())
-             ->method('show_add_tii_tutors_form')
-             ->willReturn('<form>fake form!</form>')
-             ->withConsecutive(
+            ->method('show_add_tii_tutors_form')
+            ->willReturn('<form>fake form!</form>')
+            ->withConsecutive(
                 [$this->equalTo('fakemodule'), $this->equalTo('faketutors')]
             );
 
-        // Create a stub for the SomeClass class.
+        // fake/stub a fake tii assignment
         $faketiiassignment = $this->createMock(turnitintooltwo_assignment::class);
 
-        // Configure the stub.
+        // make assignment get users by role to always return something, we
+        // check that our tii two view stub gets called with the result of this
+        // stub.
         $faketiiassignment->method('get_tii_users_by_role')
-             ->willReturn('faketutors');
+            ->willReturn('faketutors');
 
+        // create our members view class instance with the fake tii view and
+        // assignment
         $members = new members_view(null, 'fakemodule', $faketiiview, $faketiiassignment);
 
+        // test when not displaying tutor members
         $form = $members->build_add_tutors_form("foobar");
-
         $this->assertEquals('', $form);
 
+        // test when displaying tutor members
         $form = $members->build_add_tutors_form("tutors");
         $this->assertEquals('<form>fake form!</form>', $form);
     }
