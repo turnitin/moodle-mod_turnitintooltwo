de7463508819bc49ded0ebe41c702fa30d0933dc
Job Cespedes <jobcespedes@users.noreply.github.com>
2017-03-02 11:00:14 -0600

    Assign 'es_mx' to 'es' language

--- turnitintooltwo_comms.class.php
+++ turnitintooltwo_comms.class.php
@@ -168,6 +168,7 @@ class turnitintooltwo_comms {
             'fr' => 'fr',
             'fr_ca' => 'fr',
             'es' => 'es',
+            'es_mx' => 'es',
             'de' => 'de',
             'de_du' => 'de',
             'zh_cn' => 'zh_hans',
