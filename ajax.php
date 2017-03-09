f7c83770a7398ae47c6bad06db2b822a8c569990
John McGettrick <jmcgettrick@iparadigms.com>
2017-03-09 13:10:22 +0000

    Return a valid JSON response.

--- ajax.php
+++ ajax.php
@@ -243,7 +243,7 @@ switch ($action) {
             unset($_SESSION["TiiSubmissions"][$part->id]);
         }
 
-        return true;
+        return json_encode( array('success' => true) );
         break;
 
     case "get_submissions":
