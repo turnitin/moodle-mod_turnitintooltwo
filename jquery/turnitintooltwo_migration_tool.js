c6f9e5f1bc3bb8a50eeaadfef83fc52aa78b719d
David Winn <dwinn@iparadigms.com>
2017-05-02 17:26:35 +0100

    Rework logic checks and implement automatic migration.

--- jquery/turnitintooltwo_migration_tool.js
+++ jquery/turnitintooltwo_migration_tool.js
@@ -1,33 +1,45 @@
 $(document).ready(function(){
-    $.colorbox({width: 500, height: 500, inline:true, opacity: "0.7", href:"#migration_alert",
-    onLoad: function() {
-        lightBoxCloseButton();
-    },
-    onCleanup:function() {
-        $('#tii_close_bar').remove();
-        $('#migration_alert').hide();
-    }});
-    $('#migration_alert').show();
+
+    // only display the modal during a manual migration.
+    if ($("#migrate_type").data("migratetype") == 1) {
+        $.colorbox({width: 500, height: 500, inline:true, opacity: "0.7", href:"#migration_alert",
+        onLoad: function() {
+            lightBoxCloseButton();
+        },
+        onCleanup:function() {
+            $('#tii_close_bar').remove();
+            $('#migration_alert').hide();
+        }});
+        $('#migration_alert').show();
+    } else {
+        migrate($("#migrate_type").data("courseid"), $("#migrate_type").data("turnitintoolid"));
+    }
 }); 
 
 $('.migrate_link').click(function() {
-    $.ajax({
-        "dataType": 'json',
-        "type": "POST",
-        url: M.cfg.wwwroot + "/mod/turnitintooltwo/ajax.php",
-        "data": {action: "begin_migration", courseid: $(this).data("courseid"), turnitintoolid: $(this).data("turnitintoolid"), sesskey: M.cfg.sesskey},
-        success: function(data) {
-            $.colorbox.close();
-            $('#migration_alert').hide();
-            window.location.href = M.cfg.wwwroot + "/mod/turnitintooltwo/view.php?id="+data.id;
-        }
-    });
+    migrate($(this).data("courseid"), $(this).data("turnitintoolid"));
 });
 
 $('.dontmigrate_link').click(function () {
     $.colorbox.close();
     $('#migration_alert').hide();
 });
+
+function migrate(courseid, turnitintoolid) {
+    $.ajax({
+        "dataType": 'json',
+        "type": "POST",
+        url: M.cfg.wwwroot + "/mod/turnitintooltwo/ajax.php",
+        "data": {action: "begin_migration", courseid: courseid, turnitintoolid: turnitintoolid, sesskey: M.cfg.sesskey},
+        success: function(data) {
+            if ($.colorbox) {
+                $.colorbox.close();
+            }
+            $('#migration_alert').hide();
+            window.location.href = M.cfg.wwwroot + "/mod/turnitintooltwo/view.php?id="+data.id;
+        }
+    });
+}
 
 function lightBoxCloseButton() {
     $('body').append('<div id="tii_close_bar"><a href="#" onclick="$.colorbox.close(); return false;">' + M.str.turnitintooltwo.closebutton + '</a></div>');
