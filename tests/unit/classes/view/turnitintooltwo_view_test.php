324cc60c9f0710138d66c9db1f6feed558576bf0
John McGettrick <jmcgettrick@turnitin.com>
2017-05-03 10:32:55 +0100

    Add name and value to insert

--- tests/unit/classes/view/turnitintooltwo_view_test.php
+++ tests/unit/classes/view/turnitintooltwo_view_test.php
@@ -70,6 +70,8 @@ class mod_turnitintooltwo_view_testcase extends test_lib {
         if (!boolval($module)) {
             $module = new stdClass();
             $module->plugin = 'mod_turnitintool';
+            $module->name = 'version';
+            $module->version = 1;
             $DB->insert_record('config_plugins', $module);
         }
 
