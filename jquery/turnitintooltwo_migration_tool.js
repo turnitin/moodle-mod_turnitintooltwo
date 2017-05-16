12ad34e82c916617123a9a1171efa1271819a966
John McGettrick <jmcgettrick@turnitin.com>
2017-05-16 11:30:06 +0100

    Check assignment has been redirected in this session and redirect to v2 if so.

--- jquery/turnitintooltwo_migration_tool.js
+++ jquery/turnitintooltwo_migration_tool.js
@@ -1,4 +1,37 @@
 $(document).ready(function(){
+
+    // Check whether this assignment has been migrated in this session and redirect if so.
+    $.ajax({
+        "dataType": 'json',
+        "type": "POST",
+        "url": M.cfg.wwwroot + "/mod/turnitintooltwo/ajax.php",
+        "data": {
+            action: "check_migrated", 
+            turnitintoolid: $("#migrate_type").data("turnitintoolid"),
+            sesskey: M.cfg.sesskey
+        },
+        "success": function(data) {
+            if (data.migrated == true) {
+                window.location.href = M.cfg.wwwroot + "/mod/turnitintooltwo/view.php?id="+data.v2id;
+            } else {
+                displayMigrationModal();
+            }
+        }
+    });
+});
+
+$('.migrate_link').click(function() {
+    $('#asktomigrate').hide();
+    $('#migrating').show();
+    migrate($(this).data("courseid"), $(this).data("turnitintoolid"));
+});
+
+$('.dontmigrate_link').click(function () {
+    $.colorbox.close();
+    $('#migration_alert').hide();
+});
+
+function displayMigrationModal() {
     // Only display the modal during a manual migration.
     if ($("#migrate_type").data("turnitintoolid") != $("#migrate_type").data("lastasked")) {
         $.colorbox({width: 500, height: 500, inline:true, opacity: "0.7", href:"#migration_alert",
@@ -19,18 +52,7 @@ $(document).ready(function(){
 
         migrate($("#migrate_type").data("courseid"), $("#migrate_type").data("turnitintoolid"));
     }
-});
-
-$('.migrate_link').click(function() {
-    $('#asktomigrate').hide();
-    $('#migrating').show();
-    migrate($(this).data("courseid"), $(this).data("turnitintoolid"));
-});
-
-$('.dontmigrate_link').click(function () {
-    $.colorbox.close();
-    $('#migration_alert').hide();
-});
+}
 
 function migrate(courseid, turnitintoolid) {
     $.ajax({
