24172ac509c27b91a456a7ca6f8a4db774c1431a
John McGettrick <jmcgettrick@turnitin.com>
2017-06-29 16:15:01 +0100

    Add grades updated timestamp to parts after migrating

--- version.php
+++ version.php
@@ -25,7 +25,7 @@ if (empty($plugin)) {
     $plugin = new StdClass();
 }
 
-$plugin->version   = 2017052402;
+$plugin->version   = 2017062901;
 $plugin->release   = "2.7+";
 $plugin->requires  = 2014051200;
 $plugin->component = 'mod_turnitintooltwo';
