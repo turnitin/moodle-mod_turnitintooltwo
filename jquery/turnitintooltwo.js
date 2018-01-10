d421bdb4151d7d5b41d91b56f36149ee0f32e27a
David Winn <dwinn@iparadigms.com>
2018-01-10 09:45:51 +0000

    Fixes the issue where the select box would appear on top of the confirm window.

--- jquery/turnitintooltwo.js
+++ jquery/turnitintooltwo.js
@@ -543,7 +543,7 @@
         }
 
         // Show warning when changing the rubric linked to an assignment.
-        $('#id_rubric, #id_plagiarism_rubric').focus(function () {
+        $('#id_rubric, #id_plagiarism_rubric').mousedown(function () {
             if ($('input[name="instance"]').val() != '' && $('input[name="rubric_warning_seen"]').val() != 'Y') {
                 if (confirm(M.str.turnitintooltwo.changerubricwarning)) {
                     $('input[name="rubric_warning_seen"]').val('Y');
