c0915a0a33ee777e8fffa7dc9ac11f542c6e6d39
Jakob Andersson <jandersson@turnitin.com>
2023-09-14 11:53:24 +0200

    INT-18508 Supress warning with attribute flag `ReturnTypeWillChange` hides warning in PHP 8.1 and is ignored as a comment in earlier versions.`

--- sdk/soap.class.php
+++ sdk/soap.class.php
@@ -282,7 +282,8 @@ class Soap extends SoapClient {
 
         parent::__construct( $wsdl, $options );
     }
-
+    
+    #[\ReturnTypeWillChange]
     public function __doRequest($request, $location, $action, $version, $one_way = null) {
 
         $http_headers = array(
