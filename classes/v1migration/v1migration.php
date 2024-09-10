a703c7ede864e5248860b25e25922567d1beb678
Jack <jmilgate@turnitin.com>
2024-09-10 15:46:46 +0100

    Rename js modules

--- classes/v1migration/v1migration.php
+++ classes/v1migration/v1migration.php
@@ -550,7 +550,7 @@ class v1migration {
     }
 
     /**
-     * Get assignments for migrated data table. Called from ajax.php via turnitintooltwo_extra-2024032901.min.js.
+     * Get assignments for migrated data table. Called from ajax.php via turnitintooltwo_extra-2024100901.min.js.
      *
      * @global type $DB
      * @return array return array of assignments to display
